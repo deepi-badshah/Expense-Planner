c99f792e40733d7f56c61ab3e0f6b7626c13bc35
Deepanshu <b420021@iiit-bh.ac.in>
2022-10-11 16:45:45 +0530

    Transaction Row modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Futter Project",
       home: MyHomePage(),
@@ -18,7 +20,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: "New Shoes",
+      amount: 999,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Weekly Groceries",
+      amount: 500,
+      date: DateTime.now(),
+    ),
+  ];
+  MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +43,8 @@ class MyHomePage extends StatelessWidget {
         title: const Text("Home Page"),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
             width: double.infinity,
@@ -41,13 +59,44 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          const Spacer(),
-          const Card(
-            color: Colors.orange,
-            child: Text(
-              "List of Tx",
-              style: TextStyle(color: Colors.white),
-            ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        'Rs. ' + tx.amount.toString(),
+                        style:
+                            const TextStyle(color: Colors.purple, fontSize: 18),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tx.title!,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        Text(
+                          DateFormat().format(tx.date!),
+                          style: const TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
           ),
         ],
       ),
